4b1e5fe2e92ff1d77e7c4eaf5760fa64bf8b7dad
athallaz <athallahzzm@gmail.com>
2024-10-04 08:12:33 +0700

    Add files via upload

---
+++ Proyek_Analisis_Data2.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+sns.set(style='dark')
+def get_total_count_by_hour_df(hour_df):
+  hour_count_df =  hour_df.groupby(by="hours").agg({"count_cr": ["sum"]})
+  return hour_count_df
+
+def count_by_day_df(day_df):
+    day_df_count_2011 = day_df.query(str('dteday >= "2011-01-01" and dteday < "2012-12-31"'))
+    return day_df_count_2011
+
+def total_registered_df(day_df):
+   reg_df =  day_df.groupby(by="dteday").agg({
+      "registered": "sum"
+    })
+   reg_df = reg_df.reset_index()
+   reg_df.rename(columns={
+        "registered": "register_sum"
+    }, inplace=True)
+   return reg_df
+
+def total_casual_df(day_df):
+   cas_df =  day_df.groupby(by="dteday").agg({
+      "casual": ["sum"]
+    })
+   cas_df = cas_df.reset_index()
+   cas_df.rename(columns={
+        "casual": "casual_sum"
+    }, inplace=True)
+   return cas_df
+
+def sum_order (hour_df):
+    sum_order_items_df = hour_df.groupby("hours").count_cr.sum().sort_values(ascending=False).reset_index()
+    return sum_order_items_df
+
+def macem_season (day_df): 
+    season_df = day_df.groupby(by="season").count_cr.sum().reset_index() 
+    return season_df
+
+days_df = pd.read_csv('day_bersih.csv')
+hours_df = pd.read_csv('hour_bersih.csv')
+
+datetime_columns = ["dteday"]
+days_df.sort_values(by="dteday", inplace=True)
+days_df.reset_index(inplace=True)   
+
+hours_df.sort_values(by="dteday", inplace=True)
+hours_df.reset_index(inplace=True)
+
+for column in datetime_columns:
+    days_df[column] = pd.to_datetime(days_df[column])
+    hours_df[column] = pd.to_datetime(hours_df[column])
+
+min_date_days = days_df["dteday"].min()
+max_date_days = days_df["dteday"].max()
+
+min_date_hour = hours_df["dteday"].min()
+max_date_hour = hours_df["dteday"].max()
+
+with st.sidebar:
+    # Menambahkan logo
+    st.image("https://st4.depositphotos.com/6900204/24761/v/1600/depositphotos_247616236-stock-illustration-flat-banner-bicycle-rental-online.jpg")
+        # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value=min_date_days,
+        max_value=max_date_days,
+        value=[min_date_days, max_date_days])
+  
+main_df_days = days_df[(days_df["dteday"] >= str(start_date)) & 
+                       (days_df["dteday"] <= str(end_date))]
+
+main_df_hour = hours_df[(hours_df["dteday"] >= str(start_date)) & 
+                        (hours_df["dteday"] <= str(end_date))]
+
+hour_count_df = get_total_count_by_hour_df(main_df_hour)
+day_df_count_2011 = count_by_day_df(main_df_days)
+reg_df = total_registered_df(main_df_days)
+cas_df = total_casual_df(main_df_days)
+sum_order_items_df = sum_order(main_df_hour)
+season_df = macem_season(main_df_hour)
+
+#Melengkapi Dashboard dengan Berbagai Visualisasi Data
+
+st.subheader("Jam berapa penyewaan sepeda tertinggi dan terendah?")
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(25, 15))
+
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.head(5), palette=["#929591", "#929591", "#FF6347", "#929591", "#929591"], ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Hours (PM)", fontsize=25)
+ax[0].set_title("Jam dengan penyewa sepeda tertinggi", loc="center", fontsize=30)
+ax[0].tick_params(axis='y', labelsize=30)
+ax[0].tick_params(axis='x', labelsize=25)
+ 
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.sort_values(by="hours", ascending=True).head(5), palette=["#929591", "#929591", "#929591", "#929591","#FF6347"], ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Hours (AM)",  fontsize=25)
+ax[1].set_title("Jam dengan penyewa sepeda terendah", loc="center", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].tick_params(axis='y', labelsize=30)
+ax[1].tick_params(axis='x', labelsize=25)
+st.pyplot(fig)
+
+st.subheader("Di Musim Apa Penyewaan Sepeda Mencapai Puncaknya?")
+
+colors = ["#929591", "#929591", "#929591", "#FF6347"]
+fig, ax = plt.subplots(figsize=(30, 20))
+sns.barplot(
+        y="count_cr", 
+        x="season",
+        data=season_df.sort_values(by="season", ascending=False),
+        palette=colors,
+        ax=ax
+    )
+ax.set_title("Grafik Penyewa Antar Musim", loc="center", fontsize=40)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='x', labelsize=25)
+ax.tick_params(axis='y', labelsize=20)
+st.pyplot(fig)
+
